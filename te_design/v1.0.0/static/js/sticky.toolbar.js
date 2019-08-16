9c0f228172cba81af80f3d9c4bc171de19d082b1
rolzwy7 <rolzwy7@gmail.com>
2019-08-16 22:33:42 +0200

    update te

--- te_design/v1.0.0/static/js/sticky.toolbar.js
+++ te_design/v1.0.0/static/js/sticky.toolbar.js
@@ -1,55 +1,57 @@
-var toc_flag = true;
+// var TOC_FLAG = true;
 
-function stick() {
-  // $("#header").removeClass("header-transparent");
-  $("#header").addClass("header-stick");
-  $("#header").addClass("shadow");
-}
+// function stick() {
+//   // $("#header").removeClass("header-transparent");
+//   $("#header").addClass("header-stick");
+//   $("body").removeClass("header-shadow");
+// }
+//
+// function unstick() {
+//   if(TOC_FLAG != false) {
+//     $("#header").removeClass("header-stick");
+//     // $("#header").addClass("header-transparent");
+//     $("body").removeClass("header-shadow");
+//   }
+// }
+//
+// function trigger_header() {
+//   var from_top = $(window).scrollTop();
+//   var trigger_value = 30;
+//   if(from_top >= trigger_value) {
+//     console.log("stick");
+//       stick()
+//   } else {
+//       unstick();
+//       console.log("unstick");
+//   }
+// }
 
-function unstick() {
-  if(toc_flag != false) {
-    $("#header").removeClass("header-stick");
-    // $("#header").addClass("header-transparent");
-    $("#header").removeClass("shadow");
-  }
-}
+// function cloak() {
+//   $("#toc").addClass("invisible");
+//   $("#toc").removeClass("visible");
+// }
+//
+// function uncloak() {
+//   $("#toc").addClass("visible");
+//   $("#toc").removeClass("invisible");
+// }
 
-function trigger_header() {
-  var from_top = $(window).scrollTop();
-  var trigger_value = 30;
-  if(from_top >= trigger_value) {
-      stick()
-  } else {
-      unstick();
-  }
-}
-
-function cloak() {
-  $("#toc").addClass("invisible");
-  $("#toc").removeClass("visible");
-}
-
-function uncloak() {
-  $("#toc").addClass("visible");
-  $("#toc").removeClass("invisible");
-}
-
-$(window).on("scroll", function() {
-	trigger_header();
-});
-
-$(window).on("load", function() {
-  trigger_header();
-  uncloak();
-});
-
-$("#toc").on("click", function() {
-  $(this).toggleClass("is-active");
-  if(toc_flag) {
-    toc_flag = false;
-    stick();
-  } else {
-    toc_flag = true;
-    trigger_header();
-  }
-});
+// $(window).on("scroll", function() {
+// 	trigger_header();
+// });
+//
+// $(window).on("load", function() {
+//   trigger_header();
+//   uncloak();
+// });
+//
+// $("#toc").on("click", function() {
+//   $(this).toggleClass("is-active");
+//   if(TOC_FLAG) {
+//     TOC_FLAG = false;
+//     stick();
+//   } else {
+//     TOC_FLAG = true;
+//     trigger_header();
+//   }
+// });
